a5e0bd03fb8d2fd1340cdef1dbdb418287c8a674
Tesso <meidabelvine54@gmail.com>
2024-06-14 14:43:48 +0100

    basic hello world

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <section className="max-w-4xl m-auto p-4">
+          {children}
+        </section>
+      </body>
     </html>
   );
 }
