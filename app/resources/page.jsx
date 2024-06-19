ed22e832f199237681494b490179be8cbee0cebc
Tesso <meidabelvine54@gmail.com>
2024-06-19 20:03:08 +0100

    added new changes on the page

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function page() {
+
   return (
     <>
       <div>
         <categories>
           <h1>Categories</h1>
           <div>
-            {/* <select>
+            <select>
               <option value={""}> </option>
-            </select> */}
-
-            <h2>Resourses</h2>
+            </select>
+            <h2 >Resourses</h2>
           </div>
         </categories>
       </div>
