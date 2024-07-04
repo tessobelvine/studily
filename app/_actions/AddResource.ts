15ac95d1ca7f67a7cfd4e85c6960869db7ae7c74
Azem <azemchap@gmail.com>
2024-07-04 09:42:05 +0100

    working...

--- app/_actions/AddResource.ts
+++ app/_actions/AddResource.ts
@@ -3,7 +3,7 @@
 import { z } from "zod"
 import { db } from "../../lib/db"
 import fs from "fs/promises"
-import { notFound, redirect } from "next/navigation"
+import {  redirect } from "next/navigation"
 import { revalidatePath } from "next/cache"
 
 
